05ceaac5727df7cbe6e20ea7326019fa09c3e58d
Ivalin Chobanov <ivalin.chobanov@abv.bg>
2025-06-06 17:22:15 +0100

    Fixed a bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.PAGES_BASE_PATH || 'https://ivalin.com',
+    siteUrl: 'https://ivalin.com',
     generateRobotsTxt: true,
     generateIndexSitemap: false,
     robotsTxtOptions: {
