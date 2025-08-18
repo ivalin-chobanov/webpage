c682af8d0b27f36adc4001df8deb339841522830
Ivalin Chobanov <ivalin.chobanov@abv.bg>
2025-08-18 21:48:21 +0100

    chore: updated content

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           <div className="is-flex is-flex-direction-column is-align-items-center ">
             <div className="is-flex is-flex-direction-column is-align-items-start nowrap">
               <ImageItem icon={faEnvelope}>chobanov [at] ivalin [dot] com</ImageItem>
+              <ImageItem icon={faEnvelope}>I.L.Chobanov [at] sms.ed.ac.uk</ImageItem>
               <ImageItem icon={faLocationDot}>Edinburgh, United Kingdom</ImageItem>
             </div>
           </div>
@@ -32,18 +33,20 @@ export default function Home() {
             <h1 className="title is-3">Ivalin Chobanov</h1>
             <p className="subtitle is-5">PhD student @ University of Edinburgh</p>
             <p className="mb-1">
-              I am a first year PhD student at the University of Edinburgh, member of the <a href="https://rad.inf.ed.ac.uk/">Robust Autonomy and Decisions Group</a> and the <a href="https://agents-lab.org/">Autonomous Agents Research Group</a>.
-              My research focuses on Reinforcement Learning and Multi-Agent Reinforcement Learning. Topics of insterest include:
+              I am a first year PhD student at the University of Edinburgh, member of the <a href="https://assistive-autonomy.ed.ac.uk/">Centre for AI in Assistive Autonomy</a> and <a href="https://edinburgh-rl.github.io">MARBLE</a> (formerly known as <a href="https://agents-lab.org/">Autonomous Agents Research Group</a>).
+              I am supervised by <a href="https://assistive-autonomy.ed.ac.uk/project/subramanian-ramamoorthy/">Subramanian Ramamoorthy</a>, <a href="https://zhaobn.github.io/">Bonan Zhao</a> and <a href="https://ignaciocarlucho.github.io/">Ignacio Carlucho</a>.
+              My research focuses on Multi-Agent Reinforcement Learning with the goal of developing methods that enable agents to exhibit complex behavior. In particular, I am interested in:
             </p>
             <ul className="mt-0 mb-1">
-              <li>Intrinsic motivation and behavior inducing mechanisms</li>
+              <li>Cooperation under sparse reward setting</li>
+              <li>Learning task and environment abstractions to enhance learning and cooperation</li>
+              <li>Using multi-agent world models</li>
               <li>Reward-free learning and goal recognition</li>
               <li>Ad-hoc teamwork and agent modelling</li>
-              <li>Learning task abstractions</li>
             </ul>
             <p>
               I am also broadly interested in the continual learning setting, designing models with long-term memory, out-of-distribution detection, zero(few)-shot learning.
-              Outside of RL/MARL, I am interested in Generative AI, Robotics and NLP/LLMs.
+              Outside of RL/MARL, I am interested in Generative AI, Robotics and World Models.
             </p>
             <p>
               Before starting my PhD, I received a Master&apos;s degree in Artificial Intelligence from the University of Edinburgh and a
